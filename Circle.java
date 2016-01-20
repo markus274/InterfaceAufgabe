5edd65b9072339ce2c4f2a4029d04fc873188732
pfummel <greenpfum-348@yahoo.de>
2016-01-20 17:19:03 +0100

    Geometry Methoden implementiert

--- Circle.java
+++ Circle.java
@@ -1,3 +1,20 @@
 public class Circle implements Geometry {
+
+    double r;
+
+    public static double getArea() {
+    
+    double area = Math.pi() * this.r * this.r;
+    return area;
+    }
+    
+    public static double getPerimeter() {
+    
+    double perimeter = Math.pi() * 2 * this.r;
+    return perimeter;
+    }
+    
+    public static String printType() {
+    }
     
 }

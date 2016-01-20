e4d5217e134a75f520183016335be3f5627cad0e
pfummel <greenpfum-348@yahoo.de>
2016-01-20 17:19:13 +0100

    Geometry Methoden implementiert

--- Square.java
+++ Square.java
@@ -1,3 +1,19 @@
 public class Square implements Geometry {
+
+    double a,b;
+
+    public static double getArea() {
     
+    double area = this.a * this.b;
+    return area;
+    }
+    
+    public static double getPerimeter() {
+    
+    double perimeter = 2 * this.a + 2 * this.b;
+    return perimeter;
+    }
+    
+    public static String printType() {
+    }    
 }

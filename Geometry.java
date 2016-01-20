683bd4f8cf665b729dd127e003b57ac51102b6ce
pfummel <greenpfum-348@yahoo.de>
2016-01-20 17:20:12 +0100

    Kommentare hinzugefügt

--- Geometry.java
+++ Geometry.java
@@ -1,6 +1,17 @@
 public interface Geometry extends Comparable, Cloneable {
+    
+    /**
+    Berechnet Flaeche des Objektes.
+    */
     public double getArea();
+    
+    /**
+    Berechnet Umfang des Objektes.
+    */
     public double getPerimeter();
+    /**
+    Gibt Klassennamen als String zurück.
+    */
     public default String printType() {
         return this.getClass().getSimpleName();
     }

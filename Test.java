e956e1f1628e8d73edcf5d052bc88db41e784f93
pfummel <greenpfum-348@yahoo.de>
2016-01-20 17:15:57 +0100

    Test.java aus Aufgabe hinzugefügt

---
+++ Test.java
@@ -0,0 +1,18 @@
+public class Test {
+    public static void main(String[] args) {
+        Square square = new Square(42);
+        if (square.clone().getArea() != 1764.0)
+            System.err.println("square: wrong area");
+        if (square.clone().getPerimeter() != 168.0)
+            System.err.println("square: wrong perimeter");
+        if (!square.equals(square.clone()))
+            System.err.println("square: wrong equals or clone");
+        Triangle triangle = new Triangle(56);
+        if (triangle.getPerimeter() != square.getPerimeter())
+            System.err.println("triangle: wrong perimeter");
+        if (triangle.getArea() != 1357.9278331339997)
+            System.err.println("triangle: wrong area");
+        if (triangle.compareTo(square) >= 0)
+            System.err.println("wrong compareTo");
+    }
+}
